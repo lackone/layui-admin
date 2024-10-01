b2d0ffe604069454495cdd7ef3104bce6d2b036a
lackone <lackone@126.com>
2024-10-01 15:56:59 +0800

    修改

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -15,4 +15,271 @@ class IndexController extends Controller
     {
         return view('index.index', compact(''));
     }
+
+    /**
+     * 获取右边栏
+     * @param $key
+     * @return array|array[]
+     */
+    public function getSidebar($key)
+    {
+        $sidebar = [];
+
+        if (in_array($key, ['bytedance', 'kuaishou', 'wx_mini_program', 'back_manage'])) {
+            $sidebar = [
+                ['title' => '短剧小程序端', 'url' => route('wx_mini_program')],
+                ['title' => '短剧字节跳动端', 'url' => route('bytedance')],
+                ['title' => '短剧快手端', 'url' => route('kuaishou')],
+                ['title' => '短剧小程序后台管理', 'url' => route('back_manage')],
+            ];
+        } else if (in_array($key, ['mini_program_dev', 'wx_gzh_h5_dev', 'website_app_dev'])) {
+            $sidebar = [
+                ['title' => '小程序开发', 'url' => route('mini_program_dev')],
+                ['title' => '公众号H5开发', 'url' => route('wx_gzh_h5_dev')],
+                ['title' => '网站APP开发', 'url' => route('website_app_dev')],
+            ];
+        } else if (in_array($key, ['company_intro', 'contact_us', 'playlet_system'])) {
+            $sidebar = [
+                ['title' => '公司简介', 'url' => route('company_intro')],
+                ['title' => '联系我们', 'url' => route('contact_us')],
+                ['title' => '短剧系统', 'url' => route('playlet_system')],
+                ['title' => '软件著作权', 'url' => route('soft_copyright')],
+                ['title' => '授权许可协议书', 'url' => route('auth_license')],
+            ];
+        } else {
+            $sidebar = [
+                ['title' => '短剧小程序端', 'url' => route('wx_mini_program')],
+                ['title' => '短剧字节跳动端', 'url' => route('bytedance')],
+                ['title' => '短剧小程序后台管理', 'url' => route('back_manage')],
+                ['title' => '小程序开发', 'url' => route('mini_program_dev')],
+                ['title' => '公众号H5开发', 'url' => route('wx_gzh_h5_dev')],
+                ['title' => '网站APP开发', 'url' => route('website_app_dev')],
+                ['title' => '公司简介', 'url' => route('company_intro')],
+                ['title' => '联系我们', 'url' => route('contact_us')],
+                ['title' => '短剧系统', 'url' => route('playlet_system')],
+                ['title' => '软件著作权', 'url' => route('soft_copyright')],
+                ['title' => '授权许可协议书', 'url' => route('auth_license')],
+            ];
+        }
+
+        return $sidebar;
+    }
+
+    /**
+     * 短剧字节跳动端
+     */
+    public function bytedance()
+    {
+        $sidebar = $this->getSidebar('bytedance');
+        $article = Article::getArticleByCode('bytedance');
+        $title = '短剧字节跳动端';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 短剧快手端
+     */
+    public function kuaishou()
+    {
+        $sidebar = $this->getSidebar('kuaishou');
+        $article = Article::getArticleByCode('kuaishou');
+        $title = '短剧快手端';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 短剧小程序端
+     */
+    public function wxMiniProgram()
+    {
+        $sidebar = $this->getSidebar('wx_mini_program');
+        $article = Article::getArticleByCode('wx_mini_program');
+        $title = '短剧小程序端';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 短剧小程序后台管理
+     */
+    public function backManage()
+    {
+        $sidebar = $this->getSidebar('back_manage');
+        $article = Article::getArticleByCode('back_manage');
+        $title = '短剧小程序后台管理';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 小程序开发
+     */
+    public function miniProgramDev()
+    {
+        $sidebar = $this->getSidebar('mini_program_dev');
+        $article = Article::getArticleByCode('mini_program_dev');
+        $title = '小程序开发';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 公众号H5开发
+     */
+    public function wxGzhH5Dev()
+    {
+        $sidebar = $this->getSidebar('wx_gzh_h5_dev');
+        $article = Article::getArticleByCode('wx_gzh_h5_dev');
+        $title = '公众号H5开发';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 网站APP开发
+     */
+    public function websiteAppDev()
+    {
+        $sidebar = $this->getSidebar('website_app_dev');
+        $article = Article::getArticleByCode('website_app_dev');
+        $title = '网站APP开发';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 公司简介
+     */
+    public function companyIntro()
+    {
+        $sidebar = $this->getSidebar('company_intro');
+        $article = Article::getArticleByCode('company_intro');
+        $title = '公司简介';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 行业新闻
+     */
+    public function industryNews()
+    {
+        $cate_id = ArticleCategory::getArticleCategoryIdByCode('industry_news');
+        $list = Article::getListByCateId($cate_id);
+        $title = '行业新闻';
+
+        return view('index.list', compact('list', 'title'));
+    }
+
+    /**
+     * 版本更新
+     */
+    public function versionUpdate()
+    {
+        $cate_id = ArticleCategory::getArticleCategoryIdByCode('version_update');
+        $list = Article::getListByCateId($cate_id);
+        $title = '版本更新';
+
+        return view('index.list', compact('list', 'title'));
+    }
+
+    /**
+     * 联系我们
+     */
+    public function contactUs()
+    {
+        $sidebar = $this->getSidebar('contact_us');
+        $article = Article::getArticleByCode('contact_us');
+        $title = '联系我们';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 短剧系统
+     */
+    public function playletSystem()
+    {
+        $sidebar = $this->getSidebar('playlet_system');
+        $article = Article::getArticleByCode('playlet_system');
+        $title = '短剧系统';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 客户案例
+     */
+    public function customerCase()
+    {
+        $cate_id = ArticleCategory::getArticleCategoryIdByCode('customer_case');
+        $list = Article::getListByCateId($cate_id);
+        $title = '客户案例';
+
+        return view('index.list', compact('list', 'title'));
+    }
+
+    /**
+     * 问答中心
+     */
+    public function questionAnswers()
+    {
+        $cate_id = ArticleCategory::getArticleCategoryIdByCode('question_answers');
+        $list = Article::getListByCateId($cate_id);
+        $title = '问答中心';
+
+        return view('index.list', compact('list', 'title'));
+    }
+
+    /**
+     * 软件著作权
+     */
+    public function softCopyright()
+    {
+        $sidebar = $this->getSidebar('soft_copyright');
+        $article = Article::getArticleByCode('soft_copyright');
+        $title = '软件著作权';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 授权许可协议书
+     */
+    public function authLicense()
+    {
+        $sidebar = $this->getSidebar('auth_license');
+        $article = Article::getArticleByCode('auth_license');
+        $title = '授权许可协议书';
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 详情
+     */
+    public function detail(Request $request, Article $article)
+    {
+        $sidebar = $this->getSidebar('detail');
+        $title = '详情 - ' . $article['title'];
+
+        return view('index.show', compact('article', 'sidebar', 'title'));
+    }
+
+    /**
+     * 搜索
+     */
+    public function search(Request $request)
+    {
+        $params = $request->all();
+
+        $list = Article::getListByCateId(0, [
+            ['title', 'like', "%{$params['keyword']}%"]
+        ]);
+        $title = '搜索';
+
+        return view('index.list', compact('list', 'title'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,64 @@ Route::group([
     'prefix' => config('web.route.prefix'),
     'as' => config('web.route.name'),
     'namespace' => config('web.route.namespace'),
-    'middleware' => [],
+    'middleware' => ['web', 'FrontTemplate'],
 ], function () {
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+    Route::any('/', 'IndexController@index')->name('index');
 
+    //短剧字节跳动端
+    Route::any('/bytedance', 'IndexController@bytedance')->name('bytedance');
+
+    //短剧快手端
+    Route::any('/kuaishou', 'IndexController@kuaishou')->name('kuaishou');
+
+    //短剧小程序端
+    Route::any('/wx_mini_program', 'IndexController@wxMiniProgram')->name('wx_mini_program');
+
+    //短剧小程序后台管理
+    Route::any('/back_manage', 'IndexController@backManage')->name('back_manage');
+
+    //小程序开发
+    Route::any('/mini_program_dev', 'IndexController@miniProgramDev')->name('mini_program_dev');
+
+    //公众号H5开发
+    Route::any('/wx_gzh_h5_dev', 'IndexController@wxGzhH5Dev')->name('wx_gzh_h5_dev');
+
+    //网站APP开发
+    Route::any('/website_app_dev', 'IndexController@websiteAppDev')->name('website_app_dev');
+
+    //公司简介
+    Route::any('/company_intro', 'IndexController@companyIntro')->name('company_intro');
+
+    //行业新闻
+    Route::any('/industry_news', 'IndexController@industryNews')->name('industry_news');
+
+    //版本更新
+    Route::any('/version_update', 'IndexController@versionUpdate')->name('version_update');
+
+    //联系我们
+    Route::any('/contact_us', 'IndexController@contactUs')->name('contact_us');
+
+    //短剧系统
+    Route::any('/playlet_system', 'IndexController@playletSystem')->name('playlet_system');
+
+    //客户案例
+    Route::any('/customer_case', 'IndexController@customerCase')->name('customer_case');
+
+    //问答中心
+    Route::any('/question_answers', 'IndexController@questionAnswers')->name('question_answers');
+
+    //软件著作权
+    Route::any('/soft_copyright', 'IndexController@softCopyright')->name('soft_copyright');
+
+    //授权许可协议书
+    Route::any('/auth_license', 'IndexController@authLicense')->name('auth_license');
+
+    //搜索
+    Route::any('/search', 'IndexController@search')->name('search');
+
+    //详情
+    Route::any('/detail/{article}', 'IndexController@detail')->name('detail');
 });
 
 
